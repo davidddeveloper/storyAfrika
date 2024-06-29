49078377fb5d007c6d45620b6c20cf87fee22cdf
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-29 04:27:27 +0000

    Implement kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,40 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now() 
-        self.updated_at = datetime.datetime.now()
+
+        if kwargs:
+            for key, val in kwargs.items():
+                setattr(self, key, val)
+            
+            if 'id' in kwargs and kwargs.get('id') is str:
+                self.id = kwargs.get('id')
+            else:
+                self.id = str(uuid.uuid4())
+            
+            if 'created_at' in kwargs and isinstance(kwargs.get('created_at'), str):
+                self.created_at = datetime.datetime.fromisoformat(
+                    kwargs.get('created_at')
+                )
+            else:
+                self.created_at = datetime.datetime.now() 
+
+            if 'updated_at' in kwargs and isinstance(kwargs.get('updated_at'), str):
+                self.updated_at = datetime.datetime.fromisoformat(
+                    kwargs.get('updated_at')
+                )
+            else:
+                self.updated_at = datetime.datetime.now()
+    
+    def to_dict(self):
+        """ Dictionary representation of the object """
+
+        dictionary = self.__dict__.copy()
+        dictionary['updated_at'] = dictionary['updated_at'].isoformat()
+        dictionary['created_at'] = dictionary['created_at'].isoformat()
+        dictionary['__class__'] = self.__class__.__name__
+
+        return dictionary
+    
+    def __str__(self) -> str:
+        return f"[{self.__class__.__name__}.{self.id}] {self.to_dict()}"
+
