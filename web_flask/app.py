685c0d8d270671514f8aa4d7c1497c0aaff40387
davidddeveloper <programmingdonebydavid@gmail.com>
2024-07-03 17:37:36 +0000

    web flask

--- web_flask/app.py
+++ web_flask/app.py
@@ -5,14 +5,18 @@
 
 from flask import Flask, render_template
 from models.engine import storage
-
+from models.topic import Topic
+from models.story import Story
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
     all = storage.all()
-    return render_template('home.html', all=all)
+    topics = storage.all(Topic)
+    stories = storage.all(Story)
+
+    return render_template('home.html', all=all, topics=topics, stories=stories)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
