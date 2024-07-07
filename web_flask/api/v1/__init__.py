9a7cdf1376bd8acbe1eb07b16b17d8ac3347f67c
davidddeveloper <programmingdonebydavid@gmail.com>
2024-07-07 01:13:50 +0000

    remove web_flask, so this branch only focuses on engine

--- web_flask/api/v1/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-views = Blueprint('views', __name__, url_prefix='/api/v1')
-
-
-from web_flask.api.v1.comments import *
-from web_flask.api.v1.stories import *
-from web_flask.api.v1.topics import *
-from web_flask.api.v1.users import *
