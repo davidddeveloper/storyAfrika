e4b73a3fc10341ecf8007159c46da1e9cf0e7c1e
davidddeveloper <programmingdonebydavid@gmail.com>
2024-11-28 01:29:58 +0000

    change models to display as list in admin field

--- app/admin.py
+++ app/admin.py
@@ -2,13 +2,29 @@ from django.contrib import admin
 from .schema import Profile, Story, Topic, Comment
 
 # Register your models here.
-admin.site.register(Story)
-admin.site.register(Topic)
-admin.site.register(Comment)
+
+common_field = ['id', 'created_at', 'updated_at']
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'writer', 'status')
+    exclude = common_field + [ 'likes', 'topics', ]
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    exclude = common_field + ['followers']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'commenter', 'story')
+    exclude = common_field + ['likes', 'unlikes']
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'get_username', 'get_email', 'short_bio', 'about', 'avatar', 'banner', 'registration_finish')
+    exclude = common_field + ['user', 'followers']
 
     def get_username(self, obj):
         return obj.user.username
