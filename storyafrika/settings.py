e5c7002b5410b087a40005e036340960a761c24b
davidddeveloper <programmingdonebydavid@gmail.com>
2024-12-12 16:16:37 +0000

    setup mysql

--- storyafrika/settings.py
+++ storyafrika/settings.py
@@ -83,8 +83,16 @@ WSGI_APPLICATION = 'storyafrika.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        # mysql
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'storyafrika',
+        "USER": os.getenv('MYSQL_USER'),
+        "PASSWORD": os.getenv('MYSQL_PASSWORD'),
+        "HOST": 'localhost',
+        "PORT": 3306,
     }
 }
 
