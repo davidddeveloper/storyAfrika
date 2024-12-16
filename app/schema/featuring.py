b7a497f76a41f01c4a7ca69e386a3957f9b24961
davidddeveloper <programmingdonebydavid@gmail.com>
2024-12-16 09:32:08 +0000

    change featuring story to upload images to cloudinary

--- app/schema/featuring.py
+++ app/schema/featuring.py
@@ -1,5 +1,7 @@
 from django.db import models
 from tinymce.models import HTMLField
+from cloudinary.models import CloudinaryField
+import cloudinary.uploader
 from .story import Story
 
 STATUS = (
@@ -8,7 +10,8 @@ STATUS = (
 )
 class FeaturingStory(models.Model):
     caption = models.CharField(null=False, max_length=50)
-    banner = models.ImageField(upload_to="media/", null=False, default='something')
+    #banner = models.ImageField(upload_to="media/", null=False, default='something')
+    banner = CloudinaryField('image')
     story = models.ForeignKey(to=Story, null=False, related_name='has_featured', on_delete=models.CASCADE)
     intro_to_story = HTMLField()
     status = models.CharField(max_length=1, choices=STATUS, default="a")
@@ -16,6 +19,14 @@ class FeaturingStory(models.Model):
     class Meta:
         verbose_name = 'Featuring Story'
         verbose_name_plural = 'Featuring Stories'
+    
+    def save(self, *args, **kwargs):
+        """ Overrride save to upload image to Cloudinary"""
+        if self.banner and not self.pk:  # If the object is new and has an image
+            upload_result = cloudinary.uploader.upload(self.banner)
+            self.banner = upload_result['secure_url']  # Store the Cloudinary URL
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.caption
